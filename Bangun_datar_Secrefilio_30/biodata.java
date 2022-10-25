f4a4ac70f4401659a2533afce7fde5481f051bdb
Secrefilio <115141609+Secrefilio@users.noreply.github.com>
2022-10-25 14:46:02 +0700

    Add files via upload

---
+++ Bangun_datar_Secrefilio_30/biodata.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangun_datar;
+
+
+
+
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class biodata {
+public static void main(String[]args){
+String nama, alamat, status;
+int usia, uangSaku;
+double tinggi, beratBadan;
+
+nama = "Secrefilio S.A";
+alamat = "Karanganyar";
+status = "Pelajar";
+usia = 17;
+uangSaku = 20000;
+tinggi = 174;
+beratBadan = 53;
+  
+System.out.println("Nama saya " + nama);
+System.out.println("Saya tinggal di " + alamat);
+System.out.println("Saya adalah seorang " + status);
+System.out.println("saya diberi uang saku " + uangSaku);
+System.out.println("berat badan saya " + beratBadan);
+
+
+
+}
+}
+    
+
